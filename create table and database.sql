7c36804d4d461bce6c1b6832ed39d491fd9f66d8
AbdulRehman457 <140358021+AbdulRehman457@users.noreply.github.com>
2023-10-30 18:55:12 +0500

    Add files via upload

---
+++ create table and database.sql
@@ -0,0 +1,18 @@
+-- create database product;
+use product;
+-- CREATE TABLE Persons (
+-- ID int unique,
+-- FirstName varchar(255),
+-- LastName varchar(255) NOT NULL,
+-- Address varchar(255),
+-- City varchar(255)
+--   );
+
+
+-- insert into product.Persons(id, FirstName, LastName,Address,City) values(1,'Harsh','Sareen','xyz','indoreID');
+
+select * from product.Persons;
+-- insert into product.Persons (id, FirstName, LastName,Address,City) values(2,'irshaad','mohmad','xxx','germany');
+-- insert into product.Persons (id, FirstName, LastName,Address,City) values(3,'sumit','agarwal','njk','varanasi');
+-- insert into product.Persons (id, FirstName, LastName,Address,City) values(4,'tom','cruise','lop','newyork');
+-- insert into product.Persons (id, FirstName, LastName,Address,City) values(5,'vin','diesel','tre','florida')
